51f9749bec8b2b92b81313f5bee5ab6f6e452996
snapiz <jonathan.lapiquonne@gmail.com>
2022-11-14 23:58:52 +0100

    tmp

--- api/src/openapi.rs
+++ api/src/openapi.rs
@@ -2,11 +2,12 @@ use actix_web::{get, web, HttpResponse};
 use utoipa::{openapi, OpenApi};
 
 use crate::group;
+use crate::command;
 
 #[derive(OpenApi)]
 #[openapi(
     paths(group::create, group::find_all),
-    components(schemas(group::Group, group::CreateCommand))
+    components(schemas(group::Group, group::CreateCommand, command::JsonResponse))
 )]
 pub struct ApiDoc;
 
